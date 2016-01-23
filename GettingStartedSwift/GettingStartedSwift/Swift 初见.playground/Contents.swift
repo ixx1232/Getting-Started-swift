20c765348fbf8b893728f917e9271fdcb947d755
ixx <ixxcome123@gamil.com>
2016-01-23 22:14:47 +0800

    Swift 初见

---
+++ GettingStartedSwift/GettingStartedSwift/Swift 初见.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+/**
+        1> 简单值
+        2> 控制流
+        3> 函数和闭包
+        4> 对象和类
+        5> 枚举和结构体
+        6> 协议和扩展
+        7> 泛型
+*/
+
+// 通常来说,编程语言教程中的第一个程序应该在屏幕上打印"Hello , world". 在 Swift中, 可以用一行代码实现:
+print("Hello, world")
+
+// 如果你写过 C 或者 Objective-C, 那你应该很熟悉这种形式 -- 在 Swift中, 这行代码就是一个完整的程序. 你不需要为了输入输出或者字符串处理导入一个单独的库. 全局作用域中的代码会被自动当做成程序的入口点, 所以你也不需要 main 函数. 尼同样不需要在每个语句结尾写上分号.
